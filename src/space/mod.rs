2fe51cfacd6564a3dc7f0ca5b2bb76157db9b993
Jinkun Chen <i@jinkunchen.com>
2023-08-19 13:55:01 -0300

    move related functions into space file from mod

--- src/space/mod.rs
+++ src/space/mod.rs
@@ -1,8 +1,5 @@
 use crate::models::TokenOperators;
 use super::Token;
-use rand::prelude::*;
-use rand_chacha::ChaCha8Rng;
-
 pub mod space_generator;
 // pub mod subspace_generator;
 
@@ -12,48 +9,4 @@ pub trait SpaceOperator {
     fn get_center(&self) -> Vec<f64>;
     fn get_random_tokens(&self, num: i64, random_seed: i64) -> Vec<Token>;
     fn print_summary(&self);
-}
-
-pub(crate) fn get_center(tokens: Vec<Token>) -> Vec<f64> {
-    let mut sum_of_embeddings: Vec<f64> = Vec::new();
-    for token in tokens.clone() {
-        if sum_of_embeddings.len() == 0 {
-            for embedding in token.embedding.clone() {
-                sum_of_embeddings.push(embedding);
-            }
-        } else {
-            for i in 0..sum_of_embeddings.len() {
-                sum_of_embeddings[i] += token.embedding[i];
-            }
-        }
-    }
-    let mut center: Vec<f64> = Vec::new();
-    for embedding in sum_of_embeddings {
-        center.push(embedding / tokens.len() as f64);
-    }
-    center
-}
-
-pub(crate) fn find(tokens: Vec<Token>, target_words: Vec<String>) -> Vec<Token> {
-    let mut find_tokens:Vec<Token> = Vec::new();
-    for token in tokens.clone() {
-        if target_words.contains(&token.word) {
-            find_tokens.push(token);
-        }
-    }
-    find_tokens
-}
-
-pub(crate) fn get_random_tokens(tokens: Vec<Token>, num: i64, random_seed: i64) -> Vec<Token> {
-    let total_tokens = tokens.len();
-    let mut random_tokens: Vec<Token> = Vec::new();
-    // set random seed, get random tokens without replacement
-    let mut rng = ChaCha8Rng::seed_from_u64(random_seed as u64);
-    let mut random_indices: Vec<usize> = (0..total_tokens).collect();
-    random_indices.shuffle(&mut rng);
-    for i in 0..num {
-        random_tokens.push(tokens[random_indices[i as usize]].clone());
-    }
-
-    random_tokens
 }
--- src/space/space_generator.rs
+++ src/space/space_generator.rs
@@ -1,9 +1,10 @@
 use crate::models::TokenOperators;
 use crate::models::Token;
 use super::SpaceOperator;
-use super::get_center;
-use super::find;
-use super::get_random_tokens;
+
+use rand_chacha::ChaCha8Rng;
+use rand::prelude::*;
+
 
 #[derive(Clone, Debug)]
 pub struct Space {
@@ -220,4 +221,48 @@ mod test {
         let token = space.get_random_tokens(1, 1);
         assert_eq!(token.len(), 1);
     }
+}
+
+fn get_center(tokens: Vec<Token>) -> Vec<f64> {
+    let mut sum_of_embeddings: Vec<f64> = Vec::new();
+    for token in tokens.clone() {
+        if sum_of_embeddings.len() == 0 {
+            for embedding in token.embedding.clone() {
+                sum_of_embeddings.push(embedding);
+            }
+        } else {
+            for i in 0..sum_of_embeddings.len() {
+                sum_of_embeddings[i] += token.embedding[i];
+            }
+        }
+    }
+    let mut center: Vec<f64> = Vec::new();
+    for embedding in sum_of_embeddings {
+        center.push(embedding / tokens.len() as f64);
+    }
+    center
+}
+
+fn find(tokens: Vec<Token>, target_words: Vec<String>) -> Vec<Token> {
+    let mut find_tokens:Vec<Token> = Vec::new();
+    for token in tokens.clone() {
+        if target_words.contains(&token.word) {
+            find_tokens.push(token);
+        }
+    }
+    find_tokens
+}
+
+fn get_random_tokens(tokens: Vec<Token>, num: i64, random_seed: i64) -> Vec<Token> {
+    let total_tokens = tokens.len();
+    let mut random_tokens: Vec<Token> = Vec::new();
+    // set random seed, get random tokens without replacement
+    let mut rng = ChaCha8Rng::seed_from_u64(random_seed as u64);
+    let mut random_indices: Vec<usize> = (0..total_tokens).collect();
+    random_indices.shuffle(&mut rng);
+    for i in 0..num {
+        random_tokens.push(tokens[random_indices[i as usize]].clone());
+    }
+
+    random_tokens
 }

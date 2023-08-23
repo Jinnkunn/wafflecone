12af066546811b9d8876abeab8844cdb03bf71a1
Jinkun Chen <i@jinkunchen.com>
2023-08-23 15:26:30 -0300

    Enable the users to define the seed of subspace

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,7 @@ fn version() {
 }
 
 #[pyfunction]
-fn calculator(path: &str) -> Calculator {
+fn calculator(path: &str, subspace_seeds: Vec<Vec<String>>) -> Calculator {
     let data = ConceptXReader::new().read(path);
     let space = Space::new(data, None);
 
@@ -31,28 +31,31 @@ fn calculator(path: &str) -> Calculator {
     let random_sub_space = Space::new(random_token.clone(), None);
     random_sub_space.write("./random_subspace.txt");
 
-    let male_words = vec![
-        String::from("male"),
-        String::from("he"),
-        String::from("boy")
-    ];
-    let male_sub_space = Space::new(space.find(&male_words), Option::from(male_words));
-    // male_sub_space.print_summary();
-    let _male_center = male_sub_space.get_center();
+    // let male_words = vec![
+    //     String::from("male"),
+    //     String::from("he"),
+    //     String::from("boy")
+    // ];
+    // let male_sub_space = Space::new(space.find(&male_words), Option::from(male_words));
+    // // male_sub_space.print_summary();
+    // let _male_center = male_sub_space.get_center();
+    //
+    // let female_words = vec![
+    //     String::from("female"),
+    //     String::from("she"),
+    //     String::from("girl")
+    // ];
+    // let female_sub_space = Space::new(space.find(&female_words), Option::from(female_words));
+    // // female_sub_space.print_summary();
+    // let _female_center = female_sub_space.get_center();
 
-    let female_words = vec![
-        String::from("female"),
-        String::from("she"),
-        String::from("girl")
-    ];
-    let female_sub_space = Space::new(space.find(&female_words), Option::from(female_words));
-    // female_sub_space.print_summary();
-    let _female_center = female_sub_space.get_center();
+    let mut sub_spaces: Vec<Space> = Vec::new();
+    for subspace_seed in subspace_seeds {
+        let sub_space = Space::new(space.find(&subspace_seed), Option::from(subspace_seed));
+        sub_spaces.push(sub_space);
+    }
 
-    let calculator = Calculator::new(random_sub_space, vec![
-        female_sub_space,
-        male_sub_space,
-    ]);
+    let calculator = Calculator::new(random_sub_space, sub_spaces);
 
     calculator
 
